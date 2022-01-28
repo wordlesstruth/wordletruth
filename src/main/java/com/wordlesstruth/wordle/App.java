cc9216a1ab56709e598acac9bc6e37679a7e570f
Phil Neumiller <phillip.neumiller@gmail.com>
2022-01-28 01:10:57 +0000

    add logging, and better file handling mechanism

--- src/main/java/com/wordlesstruth/wordle/App.java
+++ src/main/java/com/wordlesstruth/wordle/App.java
@@ -1,17 +1,41 @@
 package com.wordlesstruth.wordle;
 
+import java.util.Set;
+
+import com.wordlesstruth.wordle.input.FileHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  */
 public final class App {
+
+    private static final String FILENAME = "words.txt";
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     private App() {
+        Set<String> words = FileHandler.getWords(FILENAME);
+
+        if (words.isEmpty()) {
+            LOGGER.error("Error getting words, check that your path is correct");
+            System.exit(1);
+        }
+
+        for (String word : words) {
+            LOGGER.info(word);
+        }
+
     }
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LOGGER.info("Starting Wordle App");
+        new App();
     }
 }
--- src/main/java/com/wordlesstruth/wordle/input/FileHandler.java
+++ src/main/java/com/wordlesstruth/wordle/input/FileHandler.java
@@ -3,6 +3,8 @@ package com.wordlesstruth.wordle.input;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -12,18 +14,40 @@ public class FileHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FileHandler.class);
 
-    public FileHandler() {
-        // intentionally blank for now
+    private static final Integer WORDLE_WORD_SIZE = 5;
+
+    private FileHandler() {
     }
 
-    public boolean openFile(String fileName) {
+    public static Set<String> getWords(String fileName) {
+        Set<String> words = new HashSet<>();
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-            stream.forEach(LOGGER::info);
+            stream.forEach(words::add);
         } catch (IOException ioe) {
             LOGGER.error("Caught an error attempting to open the file", ioe);
-            return false;
         }
 
-        return true;
+        return sanitizeWordsSet(words);
+    }
+
+    private static Set<String> sanitizeWordsSet(Set<String> lines) {
+        Set<String> words = new HashSet<>();
+        for (String line : lines) {
+
+            // First, split the line by whitespace
+            for (String entry : line.split("\\s+")) {
+                // If the entry is only alphabetical characters and the right size, we have a
+                // good hit.
+                // This could be made slightly better as it currently assumes a halfway decent
+                // attempt at providing words.
+                if (entry.length() == WORDLE_WORD_SIZE && entry.matches("[a-zA-Z]+")) {
+                    words.add(entry);
+                    break;
+                }
+            }
+
+        }
+
+        return words;
     }
 }
